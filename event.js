3b631354e4d9d7b18e77447a5f6b728cb1a09826
Romuald Falce <romuald.falce@decathlon.com>
2018-01-05 11:44:39 +0100

    Add event

--- event.js
+++ event.js
@@ -1,4 +1,5 @@
 const PG = require("pg");
+const uuidv4 = require("uuid/v4");
 
 function listBuddies() {
     const client = new PG.Client();
@@ -16,11 +17,53 @@ function listBuddies() {
       )
   }
 
-  function insertBuddies() {
+  function insertEvent(label, buddies) {
+    const uuid=uuidv4();
+    const client = new PG.Client();
+    client.connect();
+
+    return client.query(
+      "INSERT INTO events (id, label, statut) VALUES ($1, $2, $3);",
+      [uuid, label, 'OPEN']
+    )
+      .then(res => {
+        //console.log("pseudo 1:", res.rows);
+        return res;
+      })
+      .then(result => {
+        buddies.forEach(function(element) {
+          insertEventParticipants(uuid,element)});
+      })
+      .catch(e => {
+        console.warn(e.stack);
+        return e.stack;
+      }
+      )
 
   }
 
 
+function insertEventParticipants(uuid,idbuddie) {
+  const client = new PG.Client();
+  client.connect();
+
+  return client.query(
+    "INSERT INTO event_participants (event_id, user_id) VALUES ($1, $2);",
+    [uuid, idbuddie]
+  )
+    .then(res => {
+      //console.log("pseudo 1:", res.rows);
+      return res;
+    })
+    .catch(e => {
+      console.warn(e.stack);
+      return e.stack;
+    }
+    )
+}
+
+
   module.exports = {
-    listBuddies:listBuddies
+    listBuddies:listBuddies,
+    insertEvent:insertEvent
   };
